c94a11dd510418657acf1e4b25b80c2b0495e49d
Gabih <gabrielademelosantiago@outlook.com>
2023-06-14 19:43:37 -0300

    BLACK coffee

---
+++ js/script.js
@@ -0,0 +1,14 @@
+if(window.SimpleSlide) {
+
+    new SimpleSlide ({
+        slide: "fotos", //nome do atributo data-slide
+        auto: false, //slide deve passar automaticamente (ja vai automatico ñ precisa colocar )
+        time: 3000, // tempo de transição dos slides (5000 = 5 segundos)
+        nav: true,
+    });
+}
+
+
+if (window.SimpleAnime) {
+	new SimpleAnime();
+}
